fc4e5d2af7c31febf3d991a3d104efb8ff0e89b4
batuhanaydogdu <58075885+batuhanaydogdu@users.noreply.github.com>
2020-12-26 21:40:58 +0300

    basic push lines

--- app/src/main/java/com/impostors/fuwud/MainActivity.java
+++ app/src/main/java/com/impostors/fuwud/MainActivity.java
@@ -8,15 +8,16 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-    private DatabaseReference mDatabase;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+        FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference myRef=mDatabase.getReference();
 
-        mDatabase.child("carts").setValue("");
+        myRef.child("users").push().setValue(new User("mahmut","email@gmail.com"));
     }
 
 }
