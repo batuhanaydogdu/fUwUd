880bc0e22af0bc728c8c9d311f436f2187319d0b
batuhanaydogdu <58075885+batuhanaydogdu@users.noreply.github.com>
2021-01-05 23:45:16 +0300

    role login fixes

--- app/src/main/java/com/impostors/fuwud/Activity/LoginActivity.java
+++ app/src/main/java/com/impostors/fuwud/Activity/LoginActivity.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,8 +18,12 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.impostors.fuwud.R;
 
 
@@ -74,7 +79,8 @@ public class LoginActivity extends AppCompatActivity {
     public void init() {
         auth = FirebaseAuth.getInstance();
         currentUser = auth.getCurrentUser();
-        databaseReference = firebaseDatabase.getReference();
+        firebaseDatabase=FirebaseDatabase.getInstance();
+        databaseReference=firebaseDatabase.getReference();
 
         editTextEmail = findViewById(R.id.editTextEmail);
         editTextPassword = findViewById(R.id.editTextPassword);
@@ -116,10 +122,50 @@ public class LoginActivity extends AppCompatActivity {
         progressDialog = new ProgressDialog(LoginActivity.this);
         progressDialog.setMessage("Logging In...");
         progressDialog.setCancelable(false);
-        Intent to_main_intent = new Intent(LoginActivity.this, MainPageActivity.class);
-        progressDialog.show();
-        startActivity(to_main_intent);
-        finish();
+        currentUser=auth.getCurrentUser();
+        Query queryForCheckRestaurant=databaseReference.child("restaurants").orderByKey().equalTo(currentUser.getUid());
+        queryForCheckRestaurant.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    for (DataSnapshot issue : snapshot.getChildren()) {
+                        Intent to_main_intent = new Intent(LoginActivity.this, RestaurantPanelActivity.class);
+                        progressDialog.show();
+                        startActivity(to_main_intent);
+                        finish();
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
+
+
+
+        Query queryForCheckUser=databaseReference.child("users").orderByKey().equalTo(currentUser.getUid());
+        queryForCheckUser.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    for (DataSnapshot issue : snapshot.getChildren()) {
+                        Intent to_main_intent = new Intent(LoginActivity.this, MainPageActivity.class);
+                        progressDialog.show();
+                        startActivity(to_main_intent);
+                        finish();
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 
     public void signUpClicked() {
