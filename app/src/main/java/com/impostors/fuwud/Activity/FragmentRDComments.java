62ffa76c68b4b2bcaee8c27b0dbeb073768128ba
batuhanmicik <necdet.micik@isik.edu.tr>
2021-01-10 18:44:45 +0300

    Pull için commit, comment ve türevleri eklendi

--- app/src/main/java/com/impostors/fuwud/Activity/FragmentRDComments.java
+++ app/src/main/java/com/impostors/fuwud/Activity/FragmentRDComments.java
@@ -1,22 +1,96 @@
 package com.impostors.fuwud.Activity;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.impostors.fuwud.Adapter.RVCommentAdapter;
+
+import com.impostors.fuwud.Model.Comment;
+import com.impostors.fuwud.Model.Restaurant;
 import com.impostors.fuwud.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class FragmentRDComments extends Fragment {
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    Activity context;
+    RecyclerView recyclerView;
+    private RVCommentAdapter adapter;
+    FirebaseAuth auth;
+    FirebaseUser User;
+    private Button sendButton;
+    EditText commentSection;
+
+
+
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_r_d_comments, container, false);
+
+        context = getActivity();
+        sendButton = view.findViewById(R.id.sendButton);
+        recyclerView = view.findViewById(R.id.allcomments);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        auth = FirebaseAuth.getInstance();
+        User = auth.getCurrentUser();
+        commentSection = view.findViewById(R.id.commentSec);
+
+
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+               commentSection.getText().toString();
+                Comment comment = new Comment();
+                comment.setOwnerId(User.getUid());
+                
+                comment.setOwnerName(User.getDisplayName());
+
+
+
+
+            }
+            FirebaseRecyclerOptions<Comment>options = new FirebaseRecyclerOptions.Builder<Comment>()
+                    .setQuery(FirebaseDatabase.getInstance().getReference("Comment").child(User.getUid())
+                            .child("Comment"), Comment.class).build();
+
+
+
+        });
+
+
+
+
+
+
+
+
         return view;
     }
 }
