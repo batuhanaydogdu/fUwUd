4055353dcb2a4d96c1ed36b3ed391e476209530b
Mert <merttalass@gmail.com>
2021-01-10 15:11:01 +0300

    order slider eklendi

--- app/src/main/java/com/impostors/fuwud/Activity/FragmentOrder.java
+++ app/src/main/java/com/impostors/fuwud/Activity/FragmentOrder.java
@@ -26,6 +26,8 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.models.SlideModel;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -33,7 +35,10 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.impostors.fuwud.Adapter.RVOrderAdapter;
 import com.impostors.fuwud.R;
 
-public class FragmentOrder extends Fragment implements LocationListener {
+import java.util.ArrayList;
+import java.util.List;
+
+public class FragmentOrder extends Fragment/* implements LocationListener*/ {
 
     Activity context;
     RVOrderAdapter adapter;
@@ -47,9 +52,7 @@ public class FragmentOrder extends Fragment implements LocationListener {
     Location loc;
     TextView textViewCoordinate;
     private RecyclerView recyclerView;
-
-
-
+    private ImageSlider sliderRestaurant;
 
     private FirebaseAuth auth;
     private FirebaseUser currentUser;
@@ -68,9 +71,14 @@ public class FragmentOrder extends Fragment implements LocationListener {
         //FirebaseRecyclerOptions<PrevOrder> options =
         //new FirebaseRecyclerOptions.Builder<PrevOrder>().setQuery
         // (FirebaseDatabase.getInstance().getReference().child("PrevOrder").limitToFirst(5), PrevOrder.class).build();
+        List<SlideModel> slideModels = new ArrayList<>();
+        slideModels.add(new SlideModel("https://upload.wikimedia.org/wikipedia/tr/thumb/2/2e/Burger_King_logo_2020.png/200px-Burger_King_logo_2020.png","Restaurant 1"));
+        slideModels.add(new SlideModel("https://www.mcdonalds.com.tr/assets/img/ogimage.jpg","Restaurant 2"));
+        slideModels.add(new SlideModel("https://upload.wikimedia.org/wikipedia/tr/b/b8/KFC_logo.png","Restaurant 3"));
+        slideModels.add(new SlideModel("https://ubf.com.tr/blog/wp-content/uploads/2014/11/Pizza-Hut-Yeni-Logo.jpg","Restaurant 4"));
+        sliderRestaurant.setImageList(slideModels,true);
 
-
-        switchSearch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        /*switchSearch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked){
@@ -82,67 +90,46 @@ public class FragmentOrder extends Fragment implements LocationListener {
             }
         });
 
-
         buttonListRestaurants.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 if(switchSearch.isChecked()){
                     getLocation();
-
-
                     checkForPermission = ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION);
-
                     if(checkForPermission == PackageManager.PERMISSION_GRANTED) {
-
                         Intent intent_to_ListedRestaurant = new Intent(getActivity().getApplication(), ListedRestaurantActivity.class);
                         intent_to_ListedRestaurant.putExtra("latitude", currentLatitude);
                         intent_to_ListedRestaurant.putExtra("longitude", currentLongitude);
                         startActivity(intent_to_ListedRestaurant);
                     }
-
-
-
-
                 }
-
                 else{
-
-
                 }
-
-
-
             }
         });
-
-
+*/
         return view;
     }
 
 
     private void init(View view){
-        switchSearch=view.findViewById(R.id.switchSearch);
+        /*switchSearch=view.findViewById(R.id.switchSearch);
         buttonListRestaurants=view.findViewById(R.id.buttonListRestaurants);
         locationManager=(LocationManager)getContext().getSystemService(getContext().LOCATION_SERVICE);
-        textViewCoordinate=view.findViewById(R.id.textViewCoordinate);
-
+        textViewCoordinate=view.findViewById(R.id.textViewCoordinate);*/
+        sliderRestaurant = (ImageSlider) view.findViewById(R.id.sliderRestaurant);
 
         auth = FirebaseAuth.getInstance();
-        currentUser = auth.getCurrentUser();
+        /*currentUser = auth.getCurrentUser();
         firebaseDatabase=FirebaseDatabase.getInstance();
-        databaseReference=firebaseDatabase.getReference();
+        databaseReference=firebaseDatabase.getReference();*/
     }
-
 
     private void listCloseRestaurants(){
 
-
-
     }
 
-
-    private void getLocation(){
+    /*private void getLocation(){
 
         checkForPermission = ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION);
 
@@ -157,7 +144,6 @@ public class FragmentOrder extends Fragment implements LocationListener {
             if (checkGPS) {
                 if (loc == null) { // eğer bir kere loc aldıysak 2.ye gerek yok o kadar hızlı yer değiştiremez çünkü değişmeme sebebi bu
                     try {
-
                         locationManager.requestLocationUpdates(
                                 LocationManager.GPS_PROVIDER,
                                 10,// locationu değiştirmek için kaç ms gerek
@@ -166,11 +152,8 @@ public class FragmentOrder extends Fragment implements LocationListener {
                         if (locationManager != null) {
                             loc = locationManager
                                     .getLastKnownLocation(LocationManager.GPS_PROVIDER);
-
                             if (loc != null) {
                                 onLocationChanged(loc);
-
-
                             }
                         }
                     } catch (SecurityException e) {
@@ -183,7 +166,6 @@ public class FragmentOrder extends Fragment implements LocationListener {
 
     @Override
     public void onLocationChanged(@NonNull Location location) {
-
         currentLatitude = location.getLatitude();
         currentLongitude = location.getLongitude();
         textViewCoordinate.setText(currentLatitude+" q "+currentLongitude);
@@ -208,17 +190,11 @@ public class FragmentOrder extends Fragment implements LocationListener {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if(requestCode == 100){
-
             checkForPermission = ContextCompat.checkSelfPermission(getContext(),
                     Manifest.permission.ACCESS_FINE_LOCATION);
-
-
-
             if (grantResults.length > 0
                     && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
                 Toast.makeText(getContext(), "izin kabul edildi.", Toast.LENGTH_LONG).show();
-
                 locationManager = (LocationManager) getContext()
                         .getSystemService(Context.LOCATION_SERVICE);
                 boolean checkGPS = locationManager
@@ -226,7 +202,6 @@ public class FragmentOrder extends Fragment implements LocationListener {
                 if (checkGPS) {
                     if (loc == null) { // eğer bir kere loc aldıysak 2.ye gerek yok o kadar hızlı yer değiştiremez çünkü değişmeme sebebi bu
                         try {
-
                             locationManager.requestLocationUpdates(
                                     LocationManager.GPS_PROVIDER,
                                     10, // locationu değiştirmek için kaç ms gerek
@@ -238,27 +213,23 @@ public class FragmentOrder extends Fragment implements LocationListener {
 
                                 if (loc != null) {
                                     onLocationChanged(loc);
-
-
                                 }
                             }
                         } catch (SecurityException e) {
-
                         }
                     }
                 }
-
             } else {
                 Log.e("location", "izin reddedildi");
                 Toast.makeText(getContext(), "İzin reddedildi.", Toast.LENGTH_LONG).show();
             }
-
         }
-    }
+    }*/
      /*public void onStart() {
         super.onStart();
         adapter.startListening();
     }
+
     @Override
     public void onStop() {
         super.onStop();
