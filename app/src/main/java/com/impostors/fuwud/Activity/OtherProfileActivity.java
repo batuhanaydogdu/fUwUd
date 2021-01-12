45033e15b295dfec860143787d378c16b5f61960
Mert <merttalass@gmail.com>
2021-01-12 23:26:22 +0300

    DatePicker eklendi

--- app/src/main/java/com/impostors/fuwud/Activity/OtherProfileActivity.java
+++ app/src/main/java/com/impostors/fuwud/Activity/OtherProfileActivity.java
@@ -1,5 +1,6 @@
 package com.impostors.fuwud.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,15 +8,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.impostors.fuwud.Model.Restaurant;
+import com.impostors.fuwud.Model.User;
 import com.impostors.fuwud.R;
 
 public class OtherProfileActivity extends AppCompatActivity {
 
     private EditText editTextProfileName, editTextProfileSurname, editTextProfileBirth, editTextProfileEmail;
     private Button buttonChangePassword, buttonProfileUpdate;
+
+    private FirebaseAuth auth;
+    private FirebaseUser currentUser;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +39,7 @@ public class OtherProfileActivity extends AppCompatActivity {
 
         init();
 
+        setDatePicker(editTextProfileBirth);
         buttonChangePassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -39,13 +56,43 @@ public class OtherProfileActivity extends AppCompatActivity {
     }
 
     public void init() {
-        editTextProfileName= findViewById(R.id.editTextProfileName);
-        editTextProfileSurname= findViewById(R.id.editTextProfileSurname);
-        editTextProfileBirth= findViewById(R.id.editTextProfileBirth);
-        editTextProfileEmail= findViewById(R.id.editTextProfileEmail);
-        buttonChangePassword= findViewById(R.id.buttonChangePassword);
-        buttonProfileUpdate= findViewById(R.id.buttonProfileUpdate);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+        auth = FirebaseAuth.getInstance();
+        currentUser = auth.getCurrentUser();
+        editTextProfileName = findViewById(R.id.editTextProfileName);
+        editTextProfileSurname = findViewById(R.id.editTextProfileSurname);
+        editTextProfileBirth = findViewById(R.id.editTextProfileBirth);
+        editTextProfileEmail = findViewById(R.id.editTextProfileEmail);
+        buttonChangePassword = findViewById(R.id.buttonChangePassword);
+        buttonProfileUpdate = findViewById(R.id.buttonProfileUpdate);
+
+        Query queryForProfile = databaseReference.child("users").child(currentUser.getUid());
+        ValueEventListener valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                        User user = dataSnapshot.getValue(User.class);
+                        String name = user.getName();
+                        String surname = user.getSurname();
+                        String email = user.getEmail();
+
+                        editTextProfileEmail.setText(email);
+                        editTextProfileSurname.setText(surname);
+                        editTextProfileName.setText(name);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        };
+        queryForProfile.addListenerForSingleValueEvent(valueEventListener);
     }
+
 
     public void goToChangePasswordPage() {
         Intent intent = new Intent(OtherProfileActivity.this, ChangePasswordActivity.class);
@@ -53,7 +100,28 @@ public class OtherProfileActivity extends AppCompatActivity {
         finish();
     }
 
-    public void updateUserInformation(){
-        //databaseden bilgi çekilecek
+    private void setDatePicker(final TextView textView) {
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerFragment newFragment = new DatePickerFragment();
+
+                Bundle b = new Bundle();
+                b.putInt("customStyle", R.style.CustomDatePickerDialog);
+                newFragment.setArguments(b);
+
+                newFragment.setDatePickerListener(new DatePickerFragment.DatePickerListener() {
+                    @Override
+                    public void onDatePicked(String date) {
+                        textView.setText(date);
+                    }
+                });
+                newFragment.show(getSupportFragmentManager(), "datePicker");
+            }
+        });
+    }
+
+    public void updateUserInformation() {
+
     }
 }
